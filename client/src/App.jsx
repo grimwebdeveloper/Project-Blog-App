c9d1d10be054d498831bf3ca1fe053929675fec4
SHARIF IQBAL JAMIL <msijmpshaan369@gmail.com>
2025-11-02 11:43:15 +0530

    Login & Register Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import Layout from './Layout';
 import IndexPage from './pages/IndexPage';
 import { Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 const App = () => {
 	return (
@@ -9,6 +10,7 @@ const App = () => {
 			<Route path='/' element={<Layout />}>
 				<Route index element={<IndexPage />} />
 				<Route path='/login' element={<LoginPage />} />
+				<Route path='/register' element={<RegisterPage />} />
 			</Route>
 		</Routes>
 	);
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,7 +1,7 @@
-const LoginPage = () => {
+const RegisterPage = () => {
 	return (
 		<form className='max-w-sm mx-auto'>
-			<h1 className='font-bold text-2xl mb-6 text-center'>LOGIN</h1>
+			<h1 className='font-bold text-2xl mb-6 text-center'>REGISTER</h1>
 			<input
 				type='text'
 				placeholder='username'
@@ -12,9 +12,9 @@ const LoginPage = () => {
 				placeholder='password'
 				className='block mb-4 w-full border border-zinc-400 bg-zinc-200 p-1'
 			/>
-			<button className='w-full bg-zinc-600 p-1 text-white'>Login</button>
+			<button className='w-full bg-zinc-600 p-1 text-white'>Register</button>
 		</form>
 	);
 };
 
-export default LoginPage;
+export default RegisterPage;
